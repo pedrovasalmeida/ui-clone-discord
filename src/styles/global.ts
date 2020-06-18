ea5e90e63bb0ab69b877a43fca4f691b97dab63a
Pedro Henrique <pedrovasalmeida@gmail.com>
2020-06-18 04:09:01 -0300

    estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,56 @@
+/** criação do estilo global */
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  /**  */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    outline: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    font-size: 16px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  :root {
+    --primary: #36393f;
+    --secondary: #2f3136;
+    --tertiary: rgb(32,34,37);
+    --quaternary: #292b2f;
+    --quinary: #393d42;
+    --senary: #828386;
+
+    --white: #fff;
+    --gray: #8a8c90;
+    --chat-input: rgb(64,68,75);
+    --symbol: #74777a;
+
+    --notification: #f84a4b;
+    --discord: #6e86d6;
+    --mention-detail: #f9a839;
+    --mention-message: #413f3f;
+
+    --link: #5d80d6;
+
+    --rocketseat: #6633cc;
+  }
+`;
